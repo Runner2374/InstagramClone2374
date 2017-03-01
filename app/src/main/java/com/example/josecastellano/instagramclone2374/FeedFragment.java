80c102c1157cb6cd0c7c944dd7105adccb4e782a
Jose <Runner2374@gmail.com>
2017-02-28 19:56:35 -0700

    Added a feed fragment .java and .xml file

---
+++ app/src/main/java/com/example/josecastellano/instagramclone2374/FeedFragment.java
@@ -0,0 +1,21 @@
+package com.example.josecastellano.instagramclone2374;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jose on 2/28/2017.
+ */
+
+public class FeedFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+}
